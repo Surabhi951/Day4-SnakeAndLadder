c3e2afeaeca1f42cdb6de78c1c09c2f565aafa37
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-15 23:31:51 +0530

    add single player position

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,40 +1,8 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
-    static final int NO_OPTION =0;
-    static final int SNAKE_OPTION =1;
-
-    static final int LADDER_OPTION =2;
-
     public static void main(String[] args) {
-        int position = 0;
-       int diaValue = rollDie();
-        System.out.println("DiaValue is:" + diaValue);
-
-        int option = getOption();
-        System.out.println("option is:" + option);
-
-        if(option == NO_OPTION){
-            System.out.println("No option");
-        }
-        else if(option == SNAKE_OPTION ){
-            System.out.println("got a snake");
-
-        }
-        else if(option ==LADDER_OPTION ){
-            System.out.println("got a ladder");
-        }
-
-    }
-
-    static int rollDie(){
-        int random1 = (int) ((Math.random()*10) % 6);
-        return random1;
-
-    }
-
-    static int getOption(){
-        int random2 = (int) ((Math.random()*10) % 3);
-        return random2;
+        int player1Position = 0;
+        System.out.println("player1 at position: " + player1Position);
     }
 }
